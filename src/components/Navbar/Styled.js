89bd50f9f00446b9c2ac9b2a5eee8bbda7469d72
Ian Sampaio <ianosampaio@gmail.com>
2023-05-05 21:58:23 -0300

    add styles

---
+++ src/components/Navbar/Styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+    padding: 2em;
+    background: papayawhip;
+    background-color: #0093E9;
+    background-image: linear-gradient(60deg, #0093E9 0%, #80D0C7 100%);
+
+    h1{
+        font-size: 30px;
+    }
+
+    p{
+        display: flex;
+        justify-content: center;
+        padding: 1em;
+    }
+
+`;
