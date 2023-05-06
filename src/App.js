f6e4b1d0c0166a08d84e0e2110b6a21f4c6ae1c0
Ian Sampaio <ianosampaio@gmail.com>
2023-05-05 21:57:46 -0300

    connecting api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Rodape from "./components/Footer/Footer";
+import Container from "./components/Container/Container";
+
 
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Navbar/>
+      <Container/>
+      <Rodape/>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import { Wrapper } from './Styled'
+
+export default function Navbar(){
+    return (
+        <>
+            <Wrapper>
+                <h1>Weather evoryday</h1>
+            </Wrapper>
+        </>
+    )
+}
